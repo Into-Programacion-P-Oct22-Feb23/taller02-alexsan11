4f294caf527e68c0a633eefc605b7070919795d6
alexsan11 <aysanchez6@utpl.edu.ec>
2022-10-21 12:25:51 -0500

    primer comit

--- proyecto2/src/proyecto2/Proyecto2.java
+++ proyecto2/src/proyecto2/Proyecto2.java
@@ -15,6 +15,11 @@ public class Proyecto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(Ejemplo dos básico de Java)
+        System.out.println(Versionado a través de Git)
+        System.out.println(Manejado desde GitHub)
+        System.out.println(Además se usar GitHubClassroom)
+        
     }
     
 }
